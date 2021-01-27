f428d0b35465b63b1b20f655bce4b15923f0fd24
Sergey <zavyalov.sergey.a@gmail.com>
2021-01-27 15:37:23 +0300

    First coomit

---
+++ dist/pulse/mailer/smart.php
@@ -0,0 +1,44 @@
+<?php 
+
+$name = $_POST['name'];
+$phone = $_POST['phone'];
+$email = $_POST['email'];
+
+require_once('phpmailer/PHPMailerAutoload.php');
+$mail = new PHPMailer;
+$mail->CharSet = 'utf-8';
+
+// $mail->SMTPDebug = 3;                               // Enable verbose debug output
+
+$mail->isSMTP();                                      // Set mailer to use SMTP
+$mail->Host = 'smtp.yandex.ru';  // Specify main and backup SMTP servers
+$mail->SMTPAuth = true;                               // Enable SMTP authentication
+$mail->Username = 'zavialov.sergei.a';                 // РќР°С€ Р»РѕРіРёРЅ
+$mail->Password = 'sk4+PnT.8mmkPxK';                           // РќР°С€ РїР°СЂРѕР»СЊ РѕС‚ СЏС‰РёРєР°
+$mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
+$mail->Port = 465;                                    // TCP port to connect to
+ 
+$mail->setFrom('zavialov.sergei.a@yandex.ru', 'Pulse');   // РћС‚ РєРѕРіРѕ РїРёСЃСЊРјРѕ 
+$mail->addAddress('siga-spb@yandex.ru');     // Add a recipient
+//$mail->addAddress('ellen@example.com');               // Name is optional
+//$mail->addReplyTo('info@example.com', 'Information');
+//$mail->addCC('cc@example.com');
+//$mail->addBCC('bcc@example.com');
+//$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
+//$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+$mail->isHTML(true);                                  // Set email format to HTML
+
+$mail->Subject = 'Данные';
+$mail->Body    = '
+		Пользователь оставил данные <br> 
+	Имя: ' . $name . ' <br>
+	Номер телефона: ' . $phone . '<br>
+	E-mail: ' . $email . '';
+
+if(!$mail->send()) {
+    return false;
+} else {
+    return true;
+}
+
+?>
