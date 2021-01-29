8b0a1ed7390765ac5b78d2ecc05275f4265f306f
Sergey <zavyalov.sergey.a@gmail.com>
2021-01-29 22:17:02 +0300

    Add form

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,8 +1,10 @@
 
+
 const hamburger = document.querySelector('.hamburger'),
     menu = document.querySelector('.menu'),
     closeCross = document.querySelector('.menu__close'),
     closeElem = document.querySelector('.menu__link');
+
 
 
 hamburger.addEventListener('click', () => {
@@ -24,3 +26,18 @@ counters.forEach( (item, i) => {
     lines[i].style.width = item.innerHTML;
 });
 
+//Form
+
+$('form').submit(function(e) {
+    e.preventDefault();
+    $.ajax({
+        type: "POST",
+        url: "mailer/smart.php",
+        data: $(this).serialize()
+    }).done(function() {
+        $(this).find("input").val("");
+
+        $('form').trigger('reset');
+    });
+    return false;
+});
